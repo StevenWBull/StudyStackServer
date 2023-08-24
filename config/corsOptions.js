b5f75906c896551f43ab2d89f44d664443dbe5d4
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-08-24 10:12:07 -0500

    add some simple config options

---
+++ config/corsOptions.js
@@ -0,0 +1,6 @@
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+
+module.exports = corsOptions;
--- server.js
+++ server.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 
 const PORT = process.env.PORT || 5050;
 
-app.use(cors());
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
