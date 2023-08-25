3efa4de12f72d80895fbf7bfcceb8b98cbb51a4c
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-08-25 12:04:51 -0500

    add in DB connection

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
+const connectDB = require('./config/dbConn');
 
 const PORT = process.env.PORT || 5050;
+
+connectDB();
 
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
@@ -14,4 +18,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connected!');
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+});
