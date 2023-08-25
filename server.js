a38d8ed90731b6e7a94e0d3fbbc9c0d8d18379ca
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-08-25 15:08:34 -0500

    testing precommit hooks

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World!')
 });
 
 mongoose.connection.once('open', () => {
