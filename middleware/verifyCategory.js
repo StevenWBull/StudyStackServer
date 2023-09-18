a5419c9d95b555f88a42af4182d7f2b3d4de3054
Raymond Lee <raymondlee093@gmail.com>
2023-09-18 13:58:55 -0400

    modified findByID method, code clean up

--- middleware/verifyCategory.js
+++ middleware/verifyCategory.js
@@ -4,31 +4,33 @@ const User = require('../model/userSchema');
 const verifyCategories = () => {
     return async (req, res, next) => {
         // Get credientials from request body
-        const userID = req.body.user_id;
+        const userID = req.body._id;
         const newCategories_title = req.body.title;
 
-        // Create a categories array
+        // Create a categories array of title property values
         const category_title_array = newCategories_title.split(',');
         try {
             // Check users collection for a document
-            const existingDoc = await User.findOne({
-                _id: userID,
-            }).exec();
+            const userDocument = await User.findById(userID).exec();
             // Check the user document for same categories as the ones to be added
             for (const title of category_title_array) {
+                // If category title already exists for user, return error
+                // Trim whitespace and convert to lowercase for comparison
                 if (
-                    existingDoc.categories.some(
-                        (category) => category.title.trim() === title
+                    userDocument.categories.some(
+                        (category) =>
+                            category.title.trim().toLowerCase() ===
+                            title.trim().toLowerCase()
                     )
                 ) {
                     return res.status(400).json({
-                        error: `Category ${title} already exists for ${existingDoc.first_name}.`,
+                        error: 'Category already exists for user.',
                     });
                 }
             }
         } catch (error) {
             return res.status(500).json({
-                error: 'Cannot update user.',
+                error: 'Cannot add category.',
             });
         }
         next();
