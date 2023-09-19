e899c2a02f30056a8da64356e90de601686bc2f9
Raymond Lee <raymondlee093@gmail.com>
2023-09-19 17:20:17 -0400

    methods added: getCategory and getAllCategories, modified endpoint for getCategory must have userID and categoryID

--- routes/api/categoryRoutes.js
+++ routes/api/categoryRoutes.js
@@ -4,10 +4,15 @@ const verifyCategories = require('../../middleware/verifyCategory');
 const verifyCategoryToDelete = require('../../middleware/verifyCategoryToDelete');
 const categoryController = require('../../controller/categoryController');
 
+router.route('/categories/:userID').get(categoryController.getAllCategories);
+
+// To find the category subdocument within the user document we need both
+// the userID and the categoryID
 router
-    .route('/categories/:id')
-    .get(categoryController.getCategories)
-    .post(verifyCategories(), categoryController.addNewCategories)
-    .delete(verifyCategoryToDelete(), categoryController.deleteCategories);
+    .route('/user/:userID/category/:categoryID')
+    .get(categoryController.getCategory);
+
+//  .post(verifyCategories(), categoryController.addNewCategories)
+//   .delete(verifyCategoryToDelete(), categoryController.deleteCategories);
 
 module.exports = router;
