27fca74080df2fd66ed572550454f0e3eb38a04b
Raymond Lee <raymondlee093@gmail.com>
2023-09-19 19:59:44 -0400

    modified routes for get post categories, added a getAllCategories and getCategory method in categoryController

--- routes/api/categoryRoutes.js
+++ routes/api/categoryRoutes.js
@@ -4,7 +4,10 @@ const verifyCategories = require('../../middleware/verifyCategory');
 const verifyCategoryToDelete = require('../../middleware/verifyCategoryToDelete');
 const categoryController = require('../../controller/categoryController');
 
-router.route('/categories/:userID').get(categoryController.getAllCategories);
+router
+    .route('/categories/:userID')
+    .get(categoryController.getAllCategories)
+    .post(verifyCategories(), categoryController.addNewCategories);
 
 // To find the category subdocument within the user document we need both
 // the userID and the categoryID
@@ -12,7 +15,6 @@ router
     .route('/user/:userID/category/:categoryID')
     .get(categoryController.getCategory);
 
-//  .post(verifyCategories(), categoryController.addNewCategories)
 //   .delete(verifyCategoryToDelete(), categoryController.deleteCategories);
 
 module.exports = router;
