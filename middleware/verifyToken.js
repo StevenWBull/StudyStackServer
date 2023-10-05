b38acf2ec8f61c9da3dcf5e9fad2318bde05965c
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-10-05 18:25:19 -0500

    send back proper json in verifyJWT

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -3,16 +3,16 @@ const jwt = require('jsonwebtoken');
 // Middleware to verify JWT
 const verifyToken = (req, res, next) => {
     const SECRET_KEY = process.env.JWT_SECRET_KEY;
-    const token = req.header('Authorization').replace('Bearer ', '');
+    const token = req?.header('Authorization')?.replace('Bearer ', '');
 
-    if (!token) return res.status(403).send('Access Denied');
+    if (!token) return res.status(403).json({ error: 'Access Denied'});
 
     try {
         const verified = jwt.verify(token, SECRET_KEY);
         req.user = verified;
         next();
     } catch (err) {
-        res.status(400).send('Invalid Token');
+        res.status(400).json({ error: 'Invalid Token'});
     }
 };
 
