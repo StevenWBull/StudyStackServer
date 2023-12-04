e251b600f42b9d0ed84982a68b15f861527636a9
Jordan Tyler <jmattyler@hotmail.com>
2023-12-03 20:16:59 -0600

    User endpoints testing

--- test/testing.js
+++ test/testing.js
@@ -12,11 +12,14 @@ const user = {
 };
 
 const token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NGMwYWRiY2MzMTBjMWFhNzA1NzNlOSIsImVtYWlsIjoic3RldmVuQG5vLmNvbSIsImZpcnN0TmFtZSI6IlN0ZXZlbiIsImxhc3ROYW1lIjoiQnVsbCIsImlhdCI6MTcwMDYwMDkwMSwiZXhwIjoxNzAxMjA1NzAxfQ.WstPjmZzUYbUAFstXjCXjfMSwlgyauL7pbM7mMUCs5Q';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NGMwYWRiY2MzMTBjMWFhNzA1NzNlOSIsImVtYWlsIjoic3RldmVuQG5vLmNvbSIsImZpcnN0TmFtZSI6IlN0ZXZlbiIsImxhc3ROYW1lIjoiQnVsbCIsImlhdCI6MTcwMTMwNzQzOCwiZXhwIjoxNzAxOTEyMjM4fQ.QXAGAyglrHEGBi068sHt9U64z2JQjihswplcGjIkvuE';
 
 //Clean database before each run
 const cleanup = async (req, res) => {
-    await User.deleteOne({ first_name: user.first_name, last_name: user.last_name });
+    await User.deleteOne({
+        first_name: user.first_name,
+        last_name: user.last_name,
+    });
 };
 
 chai.use(chaiHttp);
@@ -33,9 +36,9 @@ describe('Auth Routes', () => {
         it('should post a valid user', (done) => {
             chai.request(app)
                 .post('/v1/auth/register')
-                .auth('bearer', token)
                 .send(user)
                 .end((err, res) => {
+                    user.id = res.body.user._id;
                     if (err) {
                         expect(res).to.have.status(500);
                     } else {
@@ -55,7 +58,6 @@ describe('Auth Routes', () => {
             };
             chai.request(app)
                 .post('/v1/auth/login')
-                .auth('bearer', token)
                 .send(req)
                 .end((err, res) => {
                     if (err) {
@@ -73,7 +75,6 @@ describe('Auth Routes', () => {
         it('/logout should return 200 and user logged out message', (done) => {
             chai.request(app)
                 .get('/v1/auth/logout')
-                .auth('bearer', token)
                 .end((err, res) => {
                     if (err) {
                         expect(res).to.have.status(500);
@@ -88,3 +89,40 @@ describe('Auth Routes', () => {
 });
 
 // User routes testing
+describe('User Routes', () => {
+    describe('GET /v1/user', () => {
+        it('should get a valid user', (done) => {
+            chai.request(app)
+                .get('/v1/user')
+                .set('Authorization', `Bearer ${token}`)
+                .send({userID: user.id})
+                .end((err, res) => {
+                    if (err) {
+                        expect(res).to.have.status(500);
+                    } else {
+                        expect(res).to.have.status(200);
+                        expect(res).to.be.json;
+                    }
+                    done();
+                });
+        });
+    });
+
+    describe('PATCH /v1/user', () => {
+        it('should get a update a valid user', (done) => {
+            chai.request(app)
+                .patch('/v1/user')
+                .set('Authorization', `Bearer ${token}`)
+                .send({userID: user.id, new_email: "test@testing.com"})
+                .end((err, res) => {
+                    if (err) {
+                        expect(res).to.have.status(500);
+                    } else {
+                        expect(res).to.have.status(200);
+                        expect(res).to.be.json;
+                    }
+                    done();
+                });
+        });
+    });
+});
